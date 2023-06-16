7aae826d7324e9da27aa494cfa95f4f455efbdb3
Chris  Fynes <chris.fynes@fxdigital.uk>
2023-06-16 14:57:53 +0100

    adjust eslint config settings

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -5,41 +5,41 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'prettier'],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-    'prettier',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "prettier",
   ],
   rules: {
-    quotes: [2, 'single', 'avoid-escape'],
-    semi: [2, 'never'],
-    'no-extra-boolean-cast': 'off',
-    'no-unused-vars': [
+    quotes: [2, "double", "avoid-escape"],
+    semi: [2, "always"],
+    "no-extra-boolean-cast": "off",
+    "no-unused-vars": [
       1,
       {
         ignoreRestSiblings: true,
-        argsIgnorePattern: 'res|next|^err',
+        argsIgnorePattern: "res|next|^err",
       },
     ],
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    'prettier/prettier': [
-      'error',
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "prettier/prettier": [
+      "warn",
       {
-        trailingComma: 'all',
-        singleQuote: true,
+        trailingComma: "all",
+        singleQuote: false,
         tabWidth: 2,
-        semi: false,
+        semi: true,
         printWidth: 100,
       },
     ],
   },
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: "babel-eslint",
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module",
   },
-}
+};
